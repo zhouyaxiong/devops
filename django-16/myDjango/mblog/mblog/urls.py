322a7a93ac409e8642ae6083c9c955b6f652c479
zyx <411235298@qq.com>
2019-03-02 15:45:54 +0800

    django架站的16课0-61代码

--- django-16/myDjango/mblog/mblog/urls.py
+++ django-16/myDjango/mblog/mblog/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include,url
+from mainsite.views import homepage,showpost
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^$', homepage),
+    url(r'^admin/', admin.site.urls),
+    url(r'^post/(\d+)$', showpost),
 ]
